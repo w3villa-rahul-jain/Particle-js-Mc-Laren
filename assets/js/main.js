1b9ceaaf98db24f6785bcb195b87172f638e147c
Rahul jain <rahul.jain@w3villa.com>
2022-09-20 15:04:49 +0530

    adding mc laren page

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+nav = document.querySelector(".navbar-section");
+toggleBtn = document.querySelector(".toggle-btn");
+
+toggleBtn.addEventListener("click", function () {
+  
+  nav.classList.toggle("collapse-toggle");
+});
+
